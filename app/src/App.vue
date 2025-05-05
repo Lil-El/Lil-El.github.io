baafe56eb39e66feb3eeb17224a424627f05b87b
Lil-El <yxd99324@qq.com>
2025-05-05 18:59:24 +0800

    compile code str to html str.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,31 +1,64 @@
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="mmm" @click="run">Click me.</div>
 </template>
 
 <script setup>
+import * as _compiler from "vue/compiler-sfc";
+
+const codeStr = `
+<template>
+  <div>{{count}}</div>
+</template>
+<script setup>
+  import { ref } from 'vue'
+  const count = ref("Hello World")
+<\/script>
+
+<style scoped>
+  div {
+    color: red;
+  }
+<\/style>`;
+
+let { descriptor } = _compiler.parse(codeStr);
+
+const scopedId = Math.random().toString(36).substring(2, 10);
+
+const scriptBlock = _compiler.compileScript(descriptor, {
+  id: scopedId,
+  genDefaultAs: true,
+  inlineTemplate: true,
+  transformAssetUrls: true,
+  sourceMap: false,
+});
+
+console.log(scriptBlock);
+
+const style = _compiler.compileStyle({
+  id: scopedId,
+  source: descriptor.styles[0].content,
+  scoped: descriptor.styles[0].scoped,
+  sourceMap: false,
+});
+console.log(style);
+
+const temp = _compiler.compileTemplate({
+  id: scopedId,
+  source: descriptor.template.content,
+  scoped: descriptor.styles[0].scoped,
+  slotted: descriptor.slotted,
+});
+console.log(temp);
+
+function run() {
+  console.log("run");
+}
+
 // TODO: 博客页面展示 lil-el 目录结构，对应的package对应一个route，该页面展示对应的package的组件
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#mmm {
+  color: red;
 }
 </style>
